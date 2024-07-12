757e0bf4e41d8df69e4d40896ed89810882edb7e
dendyyyxxxx <aliennoname228@gmail.com>
2024-07-12 19:56:10 +0500

    unit_02 1-13

--- unit_02/unit_02.js
+++ unit_02/unit_02.js
@@ -1,54 +1,120 @@
 // Task 1.
 // Создайте две переменные a = 7 и b = 9. Выведите в консоль результа умножения a на b.
-
+let a = 7;
+let b = 9;
+console.log(a*b)
 // Task 2.
 // Создайте две переменные c = 7 и d = 9. Выведите на страницу в .out-2 результат деления c на d.
-
+let c = 7;
+let d = 9;
+document.querySelector(".out-2").textContent = c/d
 // document.querySelector('.out-2').textContent = 
 
 // Task 3.
 // Создайте две переменные e = 3 и f = 5. Выведите на страницу  в .out-3 результат сложения e + f.
-
+let e = 3;
+let f = 5;
+document.querySelector(".out-3").textContent = f+e
 // document.querySelector('.out-3').textContent = 
 
 // Task 4.
 // Создайте две переменные e1 = '3' и f1 = 5. Выведите на страницу  в .out-4 результат сложения e1 + f1. Объясните разницу.
-
+let e1 = '3';
+let f1 = 5;
+document.querySelector(".out-4").textContent = e1 + f1
 // document.querySelector('.out-4').textContent = 
 
 // Task 5.
 // Создайте две переменные e2 = 3 и f2 = 0. Выведите на страницу  в .out-5 результат деления e2 на f2.
-
+let e2 = 3
+let f2 = 0
+document.querySelector(".out-5").textContent = e2/f2
 // document.querySelector('.out-5').textContent =
 
 // Task 6.
 // Создайте две переменные e3 = 3 и f3 = 'Hello'. Выведите на страницу  в .out-6 результат сложения двух переменных.
-
+let e3 = 3
+let f3 = 'Hello'
+document.querySelector(".out-6").textContent = e3+f3
 // document.querySelector('.out-6').textContent = 
 
 // Task 7.
 // Создайте две переменные e4 = 3 и f4 = 'Hello'. Выведите на страницу  в .out-7 результат умножения двух переменных.
-
+let e4 = 3;
+let f4 = 'Hello'
+document.querySelector(".out-7").textContent = e4*f4
 
 // Task 8.
-// По нажатию на кнопку .b-8 выполняется функция, которая выводит в div.out-8 то, что пользователь ввел в input.i-8.
-
+// По нажатию на кнопку .b-8 выполняется функция,
+// которая выводит в div.out-8 то, что пользователь ввел в input.i-8.
+// const f1 = (a, b) => {
+// return a + b
+// }
+// function g(a, b) {
+//     return a + b
+// }
+const t8 = () => {
+    let data = document.querySelector(".i-8").value
+    document.querySelector(".out-8").innerHTML = data
+    document.querySelector('.i-8').value = ''
+}
+document.querySelector(".b-8").onclick = t8
 // Task 9.
-// По нажатию на кнопку .b-9 выполняется функция t9, которая выводит в div.out-9 то, что пользователь ввел в input.i-9. Добавьте очистку input после нажатия кнопки.
-
+// По нажатию на кнопку .b-9 выполняется функция t9,
+// которая выводит в div.out-9 то, что пользователь ввел в input.i-9.
+// Добавьте очистку input после нажатия кнопки.
+const t9 = () => {
+    let data = document.querySelector(".i-9").value
+    document.querySelector('.out-9').innerHTML = data
+    document.querySelector('.i-9').value = ''
+}
+document.querySelector(".b-9").onclick = t9
 // Task 10.
-// По нажатию на кнопку .b-10 выполняется функция t10, которая выводит в div.out-10 то, что пользователь ввел в input.i-10 умноженное на 20.
-
-
+// По нажатию на кнопку .b-10 выполняется функция t10,
+// которая выводит в div.out-10 то, что пользователь ввел в input.i-10 умноженное на 20.
+const t10 = () => {
+    let data = document.querySelector(".i-10").value
+    document.querySelector('.out-10').innerHTML = data * 20
+    document.querySelector('.i-10').value = ''
+}
+document.querySelector(".b-10").onclick = t10
 // Task 11.
-// По нажатию на кнопку .b-11 выполняется функция t11, которая выводит на страницу в .out-11 число, которое ввел пользователь в i-11 и к нему добавленное число 55. В этой задаче мы не приводим к числу полученное из input.
-
+// По нажатию на кнопку .b-11 выполняется функция t11,
+// которая выводит на страницу в .out-11 число,
+// которое ввел пользователь в i-11 и к нему добавленное число 55.
+// В этой задаче мы не приводим к числу полученное из input.
+const t11 = () => {
+    let data = document.querySelector(".i-11").value
+    document.querySelector('.out-11').textContent = +data + 55
+    document.querySelector('.i-11').value = ''
+}
+document.querySelector(".b-11").onclick = t11
 // Task 12.
-// По нажатию на кнопку выполняется функция t12. Функция должна получить данные из input i.12-1 и i.12-2 (пользователь вводин в них фамилию и имя). После чего вывести в .out-12  строку 'Hello имя фамилия', где имя - имя пользователя и фамилия - введенная фамилия. Обращаю ваше внимание - с этого момента мы работаем строго по заданию. Hello - с большой буквы. Между Hello имя один пробел (вы же включили отображение пробелов?). Вы не добавляете "от себя" - запятые, любые другие слова и символы. Аналогичные правила действуют и дальше по курсу.
-
+// По нажатию на кнопку выполняется функция t12.
+// Функция должна получить данные из input i.12-1 и i.12-2
+// (пользователь вводин в них фамилию и имя).
+// После чего вывести в .out-12  строку 'Hello имя фамилия',
+// где имя - имя пользователя и фамилия - введенная фамилия.
+// Обращаю ваше внимание - с этого момента мы работаем строго по заданию.
+// Hello - с большой буквы. Между Hello имя один пробел
+// (вы же включили отображение пробелов?). Вы не добавляете "от себя" - запятые,
+// любые другие слова и символы. Аналогичные правила действуют и дальше по курсу.
+const t12 = () => {
+    let name = document.querySelector(".i-12-1").value
+    let surname = document.querySelector(".i-12-2").value
+    document.querySelector('.out-12').textContent = 'Hello ' + name + ' ' + surname
+}
+document.querySelector(".b-12").onclick = t12
 // Task 13.
-// При нажатии кнопки .b-13 выполняется t13. Функция получает из input.i-13-1 и .i-13-2 числа в переменные a, b. После чего в .out-13 выводит их сумму. Преобразовывайте a, b в числа!!!!
-
+// При нажатии кнопки .b-13 выполняется t13.
+// Функция получает из input.i-13-1 и .i-13-2 числа в переменные a, b.
+// После чего в .out-13 выводит их сумму. Преобразовывайте a, b в числа!!!!
+const t13 = () => {
+    let a = +document.querySelector(".i-13-1").value
+    let b = +document.querySelector(".i-13-2").value
+    document.querySelector('.out-13').textContent = a + b
+}
+document.querySelector(".b-13").onclick = t13
 // Task 14.
 // По нажатию кнопки .b-14 выполняется функция t14. Она заменяет значение value в input.i-14 на строку 'Go';
 
